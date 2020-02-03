e38824d632ed93810dd596e5fa4fcea343b738c0
akhazee <60354884+akhazee@users.noreply.github.com>
2020-02-03 12:50:10 -0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -22,5 +22,12 @@ class Book
         title = bookTitle;
     }
 
-    // Add the methods here ...
+    public String getAuthor(){
+        return author;
+    
+}
+public String getTitle(){
+        return title;
+    
+}
 }
